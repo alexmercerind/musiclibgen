5232337019446be385b73acc0e88c094954dbb83
alexmercerind <saini123hitesh@gmail.com>
2021-09-24 14:53:56 +0530

    Example: Document changes

--- examples/main.cc
+++ examples/main.cc
@@ -3,13 +3,19 @@
 #include <iostream>
 
 int32_t main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
   const char* library_paths[] = {"C:\\Users\\alexmercerind\\Music"};
   LibrarySetCachePath("C:\\Users\\alexmercerind\\.library");
   LibrarySetLibraryPaths(1, library_paths);
-  LibraryIndex([](auto completed, auto total) {
-    system("cls");
-    std::cout << completed << " / " << total << std::endl;
+  LibrarySetProgressCallback([](auto completed, auto total) {
+    std::cout << completed << " / " << total << "\n";
   });
+  LibraryCreate();
+  for (auto i = 0; i < LibraryGetTrackCount(); i++) {
+    std::cout << LibraryGetTracks()[i].album_name << "\n";
+  }
   std::cin.get();
   return EXIT_SUCCESS;
 }
